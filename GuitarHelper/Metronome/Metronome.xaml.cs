9f2e68d3f09c0c1e725263cad1d0b883a936198e
muhahaha100 <oscarbaso@gmail.com>
2019-11-06 13:44:17 -0700

    Animated Metronome
    :bee:

--- GuitarHelper/Metronome/Metronome.xaml.cs
+++ GuitarHelper/Metronome/Metronome.xaml.cs
@@ -25,6 +25,74 @@ namespace GuitarHelper.Metronome
         public Metronome()
         {
             this.InitializeComponent();
+            DispatcherTimerSetup();
+        }
+
+        DispatcherTimer dispatcherTimer;
+        DateTimeOffset startTime;
+        DateTimeOffset lastTime;
+        DateTimeOffset stopTime;
+        int timesTicked = 1;
+        int timesToTick = 60 * 3;
+        int bpm = 60;
+        bool right = true;
+
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan();
+            //IsEnabled defaults to false
+            startTime = DateTimeOffset.Now;
+            lastTime = startTime;
+            dispatcherTimer.Start();
+            //IsEnabled should now be true after calling start
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            DateTimeOffset time = DateTimeOffset.Now;
+            TimeSpan span = time - lastTime;
+            lastTime = time;
+            //Time since last tick should be very very close to Interval
+            timesTicked++;
+            if ( right )
+            {
+                Special.Angle += (bpm / 90f) * (span.Milliseconds/1000f) * 60f;
+                if ( Special.Angle >= 20 )
+                {
+                    Special.Angle = 20;
+                    right = false;
+                }
+            }
+            else
+            {
+                Special.Angle -= (bpm / 90f) * (span.Milliseconds/1000f) * 60f;
+                if ( Special.Angle <= -20 )
+                {
+                    Special.Angle = -20;
+                    right = true;
+                }
+            }
+           /* if (timesTicked > timesToTick)
+            {
+                stopTime = time;
+                dispatcherTimer.Stop();
+                //IsEnabled should now be false after calling stop
+                span = stopTime - startTime;
+            }*/
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            bpm++;
+            BPM.Text = bpm.ToString();
+        }
+
+        private void SubtractButton_Click(object sender, RoutedEventArgs e)
+        {
+            bpm--;
+            BPM.Text = bpm.ToString();
         }
     }
 }
