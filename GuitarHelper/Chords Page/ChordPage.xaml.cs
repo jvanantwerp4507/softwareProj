9f62f882ccd24ba457f29d432ab296d88fd98368
Josh V <44512420+jvanantwerp4507@users.noreply.github.com>
2019-11-23 11:25:57 -0700

    Revert "Api working"
    This reverts commit 62cfc5883dd87b164f30163ac012faf6b93b6d2c.

--- GuitarHelper/Chords Page/ChordPage.xaml.cs
+++ GuitarHelper/Chords Page/ChordPage.xaml.cs
@@ -1,20 +1,16 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,93 +22,19 @@ namespace GuitarHelper.Chords_Page
     /// </summary>
     public sealed partial class BlankPage1 : Page
     {
-        //api key
-        //ea06206162bd4e5e807cf1417cda143503fe8e69 
-        
 
+       
         public BlankPage1()
         {
             this.InitializeComponent();
-            //testing();
+           
             var a = setChoords();
             flyouts(a);
         }
 
-        private dynamic api(string search)
+        public List<String> SetKeys(bool IWantMinorKeys)
         {
-            string songJson = "";
-            // Create the web request 
-            HttpWebRequest request = WebRequest.Create($"http://api.guitarparty.com/v2/chords/?query={search}") as HttpWebRequest;
-            request.Headers["Guitarparty-Api-Key"] = "ea06206162bd4e5e807cf1417cda143503fe8e69";
-            // Get the response
-            dynamic stuff = null;
-            string a = null;
-            try
-            {
-
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-                {
-                    // Get the response stream 
-                    StreamReader reader = new StreamReader(response.GetResponseStream());
-                    // save the JSON data to a string
-                    songJson = reader.ReadToEnd();
-                     stuff = JsonConvert.DeserializeObject(songJson);
-                   
-                   
-                    string name = stuff.objects[0].image_url;
-                    chordChart.Source = new BitmapImage(new Uri(name));
-            a = stuff.objects[0].uri;
-                }
-            }
-            catch (Exception ex)
-            {
-                TextBlock error = new TextBlock();
-                error.Text = "somethin done broke but dylan cant tell if it did or didnt./n Dylan cant read this";
-                multipleChords.Children.Add(error);
-            }
-                    return a;
-        }
-
-        private void api_get_varitations(string ChordID)
-        {
-            string songJson = "";
-            // Create the web request 
-            HttpWebRequest request = WebRequest.Create($"http://api.guitarparty.com{ChordID}?variations=true") as HttpWebRequest;
-            request.Headers["Guitarparty-Api-Key"] = "ea06206162bd4e5e807cf1417cda143503fe8e69";
-            // Get the response
-            try
-            {
-
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {
-                // Get the response stream 
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                // save the JSON data to a string
-                songJson = reader.ReadToEnd();
-                dynamic stuff = JsonConvert.DeserializeObject(songJson);
-                    int i = 0;
-                    foreach (var item in stuff.objects)
-                    {
-                        Image a = new Image();
- string chord = stuff.objects[i].image_url;
-                        a.Source = new BitmapImage(new Uri(chord));
-                        multipleChords.Children.Add(a);
-                        a.Height = 550;
-                        //a.Stretch = Stretch.UniformToFill;
-                        i++;
-
-                    }
-                  
-            }
-            } catch (Exception ex){
-
-            }
-        }
-
-        #region dropdown of chords
-        public List<String> SetKeys(bool? IWantMinorKeys)
-        {
-            if (IWantMinorKeys == true)
+            if (IWantMinorKeys)
             {
                 List<String> MinorKeysList = new List<String>
             {
@@ -131,19 +53,7 @@ namespace GuitarHelper.Chords_Page
 
             };
                 return MinorKeysList;
-            } else if (IWantMinorKeys == null)
-            {
-                List<String> openList = new List<String>
-            {
-              "C",
-              "B",
-              "G",
-              "D",
-              "A",
-              "E"
-                };
-                return openList;
-            }  else
+            } else
             {
 
             List<String> MajorKeysList = new List<String>
@@ -170,22 +80,22 @@ namespace GuitarHelper.Chords_Page
         {
             List<Choord> choordList = new List<Choord>
             {
-               //new Choord("C", "open", null, "Chords Page/Guitar Choords/Choords/C choord.png", )
+                new Choord("C#","A",true),
+                new Choord("D#","B",false),
+                new Choord("E#","D",true),
+                new Choord("F#","C",true),
+                new Choord("H#","G",true)
             };
             return choordList;
         }
 
-       
         public void flyouts(List<Choord> choordList)
         {
             MenuFlyoutSubItem major = new MenuFlyoutSubItem();
             MenuFlyoutSubItem minor = new MenuFlyoutSubItem();
-            MenuFlyoutSubItem open = new MenuFlyoutSubItem();
             major.Text = "Major Keys";
             minor.Text = "Minor Keys";
-            open.Text = "Open";
             Flyout.Items.Add(major);
-            Flyout.Items.Add(open);
             Flyout.Items.Add(minor);
             int i = 0;
             //for all the major keys
@@ -208,40 +118,24 @@ namespace GuitarHelper.Chords_Page
 
             }
 
-            foreach (String Key in SetKeys(null))
-            {
-                MenuFlyoutItem key = new MenuFlyoutItem();
-                key.Text = Key;
-                open.Items.Add(key);
-            }
-
 
             foreach (Choord c in choordList)
             {
-                if (c.IsMinor == null)
+                if (c.IsMinor)
                 {
                    
-                MenuFlyoutItem item = new MenuFlyoutItem();
-                item.Text = $"{c.Choordname} in {c.KeyName} key";
-                    open.Items.Add(item);
+                MenuFlyoutItem mn = new MenuFlyoutItem();
+                mn.Text = $"{c.Choordname} in {c.KeyName} key";
+                    var a = c.KeyName;
+                  
                 
-                } 
+                } else
+                {
+
+                }
             }
         }
 
-        #endregion
-
-        private void search_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            multipleChords.Children.Clear();
-            var a = api(chordSearch.Text);
-            if (a != null)
-            {
-
-        api_get_varitations(a);
-            }
-
-        }
     }
 
     public class Choord
@@ -249,9 +143,9 @@ namespace GuitarHelper.Chords_Page
         public String Choordname { get; set; }
         public String KeyName { get; set; }
 
-        public bool? IsMinor { get; set; }
+        public bool IsMinor { get; set; }
 
-        public Choord(string choordname, string keyName, bool? isMinor, String ImagePath, String AudioPath)
+        public Choord(string choordname, string keyName, bool isMinor)
         {
             Choordname = choordname;
             KeyName = keyName;
